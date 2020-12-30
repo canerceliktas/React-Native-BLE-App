058aa33cb8f8079edb003f14be165a74cc4e4ff8
caner.celiktas <celiktascaner@gmail.com>
2020-12-30 16:09:20 +0300

    Create DeviceList.js

---
+++ Components/DeviceList.js
@@ -0,0 +1,13 @@
+import React from "react";
+import { Text, StyleSheet, TouchableOpacity } from "react-native";
+
+
+
+const DeviceList = () => {
+    return(
+        <Text>Bu başka bir sayfadan component</Text>
+
+    )
+};
+
+export {DeviceList};
